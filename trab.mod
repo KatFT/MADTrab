22d4a8a6b5fcaa81ef33780fc0a456a51dcfc45d
Catarina Teixeira <71704578+KatFT@users.noreply.github.com>
2021-03-28 18:26:48 +0100

    avanços (Duarte é roto)

--- trab.mod
+++ trab.mod
@@ -1,14 +1,14 @@
-param nI #nº de meses
-set I := 1..nI # conjunto de I's
+param nI; #nº de meses
+set I := 1..nI; # conjunto de I's
 
 set PROD;
 set TRAB;
 
 param Limit;
-param mars {i in I,p in PROD}; #trata do mars por ano
-param mercury {i in I,p in PROD}; #trata do mercurio por ano
-param venus {i in I,p in PROD}; #trata de venus por ano
-param d {p in PROB,t in TRAB};	#trata da parte da produçao
+param mars {i in I,p in PROD}>=0; #trata do mars por ano
+param mercury {i in I,p in PROD}>=0; #trata do mercurio por ano
+param venus {i in I,p in PROD}>=0; #trata de venus por ano
+param d {p in PROD,t in TRAB}>=0;	#trata da parte da produçao
 
 
 var x{i in I, p in PROD} >=0; #vai somar as marmeladas a cada mes
@@ -18,6 +18,18 @@ var y{p in PROD, t in TRAB}>=0; #vai ser usada para a restriçao da produçao
 #param hij{p in PROD,t in TRAB}:=  min{ii in D, jj in R} min(o[ii,jj], p[ii,jj]);
 
 maximize revenue:
-	sum {i int I} (sum { p in PROD} mars[i,p]*venus[i,p]*mercury[i,p]);
+	sum {i in I} (sum { p in PROD} mars[i,p]*venus[i,p]*mercury[i,p]*x[i,p]);
 
 #restriçoes
+LMars {i in I} : sum {p in PROD} mars[i,p]*x[i,p] <= Limit;
+LMercury {i in I}: sum {p in PROD} mercury[i,p]*x[i,p] <= Limit;
+LVenus {i in I}: sum {p in PROD} venus[i,p]*x[i,p] <= Limit;
+
+
+
+
+
+solve;
+end;
+
+
---
+++ trab.sol
@@ -0,0 +1,105 @@
+Problem:    trab
+Rows:       37
+Columns:    36
+Non-zeros:  144
+Status:     OPTIMAL
+Objective:  revenue = 8049000 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 revenue      B      8.049e+06                             
+     2 LMars[1]     NU          1000                        1000           104 
+     3 LMars[2]     NU          1000                        1000           125 
+     4 LMars[3]     NU          1000                        1000           224 
+     5 LMars[4]     NU          1000                        1000           384 
+     6 LMars[5]     NU          1000                        1000           384 
+     7 LMars[6]     NU          1000                        1000           800 
+     8 LMars[7]     NU          1000                        1000          1575 
+     9 LMars[8]     B        866.667                        1000 
+    10 LMars[9]     NU          1000                        1000           875 
+    11 LMars[10]    NU          1000                        1000           759 
+    12 LMars[11]    NU          1000                        1000           400 
+    13 LMars[12]    NU          1000                        1000           144 
+    14 LMercury[1]  B        347.826                        1000 
+    15 LMercury[2]  B        142.857                        1000 
+    16 LMercury[3]  B        210.526                        1000 
+    17 LMercury[4]  B        285.714                        1000 
+    18 LMercury[5]  B        285.714                        1000 
+    19 LMercury[6]  B            400                        1000 
+    20 LMercury[7]  B        636.364                        1000 
+    21 LMercury[8]  B        466.667                        1000 
+    22 LMercury[9]  B        555.556                        1000 
+    23 LMercury[10] B        534.884                        1000 
+    24 LMercury[11] B        666.667                        1000 
+    25 LMercury[12] B        545.455                        1000 
+    26 LVenus[1]    B        565.217                        1000 
+    27 LVenus[2]    B        714.286                        1000 
+    28 LVenus[3]    B        736.842                        1000 
+    29 LVenus[4]    B        761.905                        1000 
+    30 LVenus[5]    B        761.905                        1000 
+    31 LVenus[6]    B            800                        1000 
+    32 LVenus[7]    B        818.182                        1000 
+    33 LVenus[8]    NU          1000                        1000          2275 
+    34 LVenus[9]    B        777.778                        1000 
+    35 LVenus[10]   B        767.442                        1000 
+    36 LVenus[11]   B        666.667                        1000 
+    37 LVenus[12]   B        545.455                        1000 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x[1,R]       NL             0             0                         -80 
+     2 x[1,C]       B        43.4783             0               
+     3 x[1,I]       NL             0             0                         -80 
+     4 x[2,R]       NL             0             0                        -550 
+     5 x[2,C]       B        28.5714             0               
+     6 x[2,I]       NL             0             0                        -300 
+     7 x[3,R]       NL             0             0                       -1540 
+     8 x[3,C]       B        26.3158             0               
+     9 x[3,I]       NL             0             0                       -2480 
+    10 x[4,R]       NL             0             0                       -2940 
+    11 x[4,C]       B        23.8095             0               
+    12 x[4,I]       NL             0             0                       -5280 
+    13 x[5,R]       NL             0             0                       -1200 
+    14 x[5,C]       B        23.8095             0               
+    15 x[5,I]       NL             0             0                       -5080 
+    16 x[6,R]       NL             0             0                        -960 
+    17 x[6,C]       B             20             0               
+    18 x[6,I]       NL             0             0                      -14875 
+    19 x[7,R]       NL             0             0                      -13410 
+    20 x[7,C]       B        18.1818             0               
+    21 x[7,I]       NL             0             0                      -26722 
+    22 x[8,R]       NL             0             0                      -22935 
+    23 x[8,C]       NL             0             0                      -31000 
+    24 x[8,I]       B        13.3333             0               
+    25 x[9,R]       NL             0             0                       -7250 
+    26 x[9,C]       B        22.2222             0               
+    27 x[9,I]       NL             0             0                       -8250 
+    28 x[10,R]      NL             0             0                       -6690 
+    29 x[10,C]      B        23.2558             0               
+    30 x[10,I]      NL             0             0                      -10770 
+    31 x[11,R]      NL             0             0                       -3300 
+    32 x[11,C]      B        33.3333             0               
+    33 x[11,I]      NL             0             0                       -4375 
+    34 x[12,R]      NL             0             0                        -840 
+    35 x[12,C]      NL             0             0                        -880 
+    36 x[12,I]      B        45.4545             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 2.27e-13 on row 4
+        max.rel.err = 1.14e-16 on row 10
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 1.46e-11 on column 20
+        max.rel.err = 2.14e-16 on column 8
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output

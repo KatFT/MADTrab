32bdd40cf15cca5bfd653b22ac319a60246fffaf
Catarina Teixeira <71704578+KatFT@users.noreply.github.com>
2021-03-29 17:59:27 +0100

    falta as restriçoes da tabela do clean,cook,pack
    Segundo o que foi feito a preguiçosa pela romi, isto sem a restriçao da outra tabela supostamente esta bem, agora o q temos de ver a merda das outras limitaçoes

--- trab.sol
+++ trab.sol
@@ -1,74 +1,101 @@
 Problem:    trab
-Rows:       10
+Rows:       37
 Columns:    36
 Non-zeros:  144
 Status:     OPTIMAL
-Objective:  revenue = 4070000 (MAXimum)
+Objective:  revenue = 33540.58727 (MAXimum)
 
    No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 revenue      B       4.07e+06                             
-     2 LMars[R]     B          312.5                        1000 
-     3 LMars[C]     NU          1000                        1000          1575 
-     4 LMars[I]     B        866.667                        1000 
-     5 LMercury[R]  B          687.5                        1000 
-     6 LMercury[C]  B        636.364                        1000 
-     7 LMercury[I]  B        466.667                        1000 
-     8 LVenus[R]    NU          1000                        1000           220 
-     9 LVenus[C]    B        818.182                        1000 
-    10 LVenus[I]    NU          1000                        1000          2275 
+     1 revenue      B        33540.6                             
+     2 LMars[1]     NU          1000                        1000       1.28571 
+     3 LMars[2]     NU          1000                        1000           2.7 
+     4 LMars[3]     NU          1000                        1000           2.7 
+     5 LMars[4]     NU          1000                        1000           2.9 
+     6 LMars[5]     NU          1000                        1000       1.48824 
+     7 LMars[6]     NU          1000                        1000       0.91954 
+     8 LMars[7]     NU          1000                        1000             1 
+     9 LMars[8]     NU          1000                        1000             1 
+    10 LMars[9]     NU          1000                        1000       1.64706 
+    11 LMars[10]    NU          1000                        1000           2.9 
+    12 LMars[11]    NU          1000                        1000           2.7 
+    13 LMars[12]    NU          1000                        1000           2.6 
+    14 LMercury[1]  B        714.286                        1000 
+    15 LMercury[2]  B            700                        1000 
+    16 LMercury[3]  B            700                        1000 
+    17 LMercury[4]  B            900                        1000 
+    18 LMercury[5]  B        811.765                        1000 
+    19 LMercury[6]  B        632.184                        1000 
+    20 LMercury[7]  NU          1000                        1000             1 
+    21 LMercury[8]  NU          1000                        1000             1 
+    22 LMercury[9]  NU          1000                        1000       1.23529 
+    23 LMercury[10] B            900                        1000 
+    24 LMercury[11] B            700                        1000 
+    25 LMercury[12] B            600                        1000 
+    26 LVenus[1]    NU          1000                        1000       1.42857 
+    27 LVenus[2]    B           1000                        1000 
+    28 LVenus[3]    B           1000                        1000 
+    29 LVenus[4]    B           1000                        1000 
+    30 LVenus[5]    NU          1000                        1000       1.32353 
+    31 LVenus[6]    NU          1000                        1000       1.71264 
+    32 LVenus[7]    NU          1000                        1000             1 
+    33 LVenus[8]    NU          1000                        1000             1 
+    34 LVenus[9]    B        882.353                        1000 
+    35 LVenus[10]   B           1000                        1000 
+    36 LVenus[11]   B           1000                        1000 
+    37 LVenus[12]   B           1000                        1000 
 
    No. Column name  St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 x[1,R]       NL             0             0                       -1680 
-     2 x[1,C]       NL             0             0                      -33833 
-     3 x[1,I]       NL             0             0                      -20750 
-     4 x[2,R]       NL             0             0                       -1500 
-     5 x[2,C]       NL             0             0                      -50750 
-     6 x[2,I]       NL             0             0                      -20550 
-     7 x[3,R]       NL             0             0                       -1500 
-     8 x[3,C]       NL             0             0                      -51338 
-     9 x[3,I]       NL             0             0                      -20750 
-    10 x[4,R]       NL             0             0                       -1300 
-    11 x[4,C]       NL             0             0                      -50022 
-    12 x[4,I]       NL             0             0                      -20350 
-    13 x[5,R]       NL             0             0                       -2200 
-    14 x[5,C]       NL             0             0                      -50022 
-    15 x[5,I]       NL             0             0                      -20150 
-    16 x[6,R]       B          31.25             0               
-    17 x[6,C]       NL             0             0                      -38750 
-    18 x[6,I]       NL             0             0                      -43750 
-    19 x[7,R]       NL             0             0                        -520 
-    20 x[7,C]       B        18.1818             0               
-    21 x[7,I]       NL             0             0                      -47372 
-    22 x[8,R]       NL             0             0                        -330 
-    23 x[8,C]       NL             0             0                      -18750 
-    24 x[8,I]       B        13.3333             0               
-    25 x[9,R]       NL             0             0                        -700 
-    26 x[9,C]       NL             0             0                      -31500 
-    27 x[9,I]       NL             0             0                      -27500 
-    28 x[10,R]      NL             0             0                       -1300 
-    29 x[10,C]      NL             0             0                      -35088 
-    30 x[10,I]      NL             0             0                      -33500 
-    31 x[11,R]      NL             0             0                       -1500 
-    32 x[11,C]      NL             0             0                      -35250 
-    33 x[11,I]      NL             0             0                      -28500 
-    34 x[12,R]      NL             0             0                       -1600 
-    35 x[12,C]      NL             0             0                      -29500 
-    36 x[12,I]      NL             0             0                      -24132 
+     1 x[1,R]       B        71.4286             0               
+     2 x[1,C]       NL             0             0                    -4.14286 
+     3 x[1,I]       B        14.2857             0               
+     4 x[2,R]       B            100             0               
+     5 x[2,C]       NL             0             0                       -29.5 
+     6 x[2,I]       NL             0             0                         -13 
+     7 x[3,R]       B            100             0               
+     8 x[3,C]       NL             0             0                       -28.6 
+     9 x[3,I]       NL             0             0                         -14 
+    10 x[4,R]       B            100             0               
+    11 x[4,C]       NL             0             0                       -35.8 
+    12 x[4,I]       NL             0             0                         -16 
+    13 x[5,R]       B        29.4118             0               
+    14 x[5,C]       NL             0             0                    -18.8588 
+    15 x[5,I]       B        35.2941             0               
+    16 x[6,R]       B        11.4943             0               
+    17 x[6,C]       NL             0             0                    -4.48276 
+    18 x[6,I]       B        25.2874             0               
+    19 x[7,R]       B        36.7647             0               
+    20 x[7,C]       B        7.35294             0               
+    21 x[7,I]       B        3.67647             0               
+    22 x[8,R]       B        32.1543             0               
+    23 x[8,C]       B        7.71704             0               
+    24 x[8,I]       B        4.50161             0               
+    25 x[9,R]       B        47.0588             0               
+    26 x[9,C]       B        11.7647             0               
+    27 x[9,I]       NL             0             0                    -6.47059 
+    28 x[10,R]      B            100             0               
+    29 x[10,C]      NL             0             0                       -25.7 
+    30 x[10,I]      NL             0             0                         -17 
+    31 x[11,R]      B            100             0               
+    32 x[11,C]      NL             0             0                         -11 
+    33 x[11,I]      NL             0             0                       -12.5 
+    34 x[12,R]      B            100             0               
+    35 x[12,C]      NL             0             0                         -12 
+    36 x[12,I]      NL             0             0                       -11.2 
 
 Karush-Kuhn-Tucker optimality conditions:
 
-KKT.PE: max.abs.err = 1.14e-13 on row 3
-        max.rel.err = 5.68e-17 on row 3
+KKT.PE: max.abs.err = 2.27e-13 on row 8
+        max.rel.err = 1.14e-16 on row 8
         High quality
 
 KKT.PB: max.abs.err = 0.00e+00 on row 0
         max.rel.err = 0.00e+00 on row 0
         High quality
 
-KKT.DE: max.abs.err = 9.09e-13 on column 16
-        max.rel.err = 6.46e-17 on column 16
+KKT.DE: max.abs.err = 2.84e-14 on column 20
+        max.rel.err = 1.35e-16 on column 26
         High quality
 
 KKT.DB: max.abs.err = 0.00e+00 on row 0

9cd7b5468bd0187e5f6425ca56571cdcf3ecc324
Catarina Teixeira <71704578+KatFT@users.noreply.github.com>
2021-03-29 18:34:56 +0100

    correçao de algumas merdas

--- trab.mod
+++ trab.mod
@@ -12,20 +12,23 @@ param d {p in PROD,t in TRAB};	#trata da parte da produçao
 #param maxi = sum{t in TRAB,p in PROD} d[p,t];
 
 
+
 var x{i in I,p in PROD} >=0; #vai somar as marmeladas a cada mes
-var y{p in PROD,t in TRAB}>=0,<=d[p,t]; #vai ser usada para a restriçao da produçao
+var x1{i in I,p in PROD} >=0; #vai somar as marmeladas a cada mes
+var x2{i in I,p in PROD} >=0; #vai somar as marmeladas a cada mes
+var y{p in PROD,t in TRAB}>=0; #vai ser usada para a restriçao da produçao
 
 
 
 #param hij{p in PROD,t in TRAB}:=  min{ii in D, jj in R} min(o[ii,jj], p[ii,jj]);
 
 maximize revenue:
-	sum {i in I} (sum { p in PROD} mars[i,p]*x[i,p] + sum { p in PROD} venus[i,p]*x[i,p] +  sum { p in PROD} mercury[i,p]*x[i,p] );
+	sum {i in I} (sum { p in PROD} mars[i,p]*x[i,p] + sum { p in PROD} venus[i,p]*x1[i,p] +  sum { p in PROD} mercury[i,p]*x2[i,p] );
 
 #restriçoes
-LMars {i in I} : sum {p in PROD} mars[i,p]*x[i,p] <= Limit;
-LMercury {i in I}: sum {p in PROD} mercury[i,p]*x[i,p] <= Limit;
-LVenus {i in I}: sum {p in PROD} venus[i,p]*x[i,p] <= Limit;
+LMars {i in I} : sum {p in PROD} x[i,p] <= Limit;
+LMercury {i in I}: sum {p in PROD} x1[i,p] <= Limit;
+LVenus {i in I}: sum {p in PROD} x2[i,p] <= Limit;
 
 #CapR {p in PROD,t in TRAB} :(sum {i in I} mars[i,p]+sum {i in I} mercury[i,p]+sum {i in I} venus[i,p])*y[p,t]<= 0;
 #CapC {i in I} :sum {p in PROD} mars[i,p]+sum {p in PROD} mercury[i,p]+sum {p in PROD} venus[i,p]<= ;
--- trab.sol
+++ trab.sol
@@ -1,101 +1,173 @@
 Problem:    trab
 Rows:       37
-Columns:    36
-Non-zeros:  144
+Columns:    108
+Non-zeros:  216
 Status:     OPTIMAL
-Objective:  revenue = 33540.58727 (MAXimum)
+Objective:  revenue = 1127000 (MAXimum)
 
    No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 revenue      B        33540.6                             
-     2 LMars[1]     NU          1000                        1000       1.28571 
-     3 LMars[2]     NU          1000                        1000           2.7 
-     4 LMars[3]     NU          1000                        1000           2.7 
-     5 LMars[4]     NU          1000                        1000           2.9 
-     6 LMars[5]     NU          1000                        1000       1.48824 
-     7 LMars[6]     NU          1000                        1000       0.91954 
-     8 LMars[7]     NU          1000                        1000             1 
-     9 LMars[8]     NU          1000                        1000             1 
-    10 LMars[9]     NU          1000                        1000       1.64706 
-    11 LMars[10]    NU          1000                        1000           2.9 
-    12 LMars[11]    NU          1000                        1000           2.7 
-    13 LMars[12]    NU          1000                        1000           2.6 
-    14 LMercury[1]  B        714.286                        1000 
-    15 LMercury[2]  B            700                        1000 
-    16 LMercury[3]  B            700                        1000 
-    17 LMercury[4]  B            900                        1000 
-    18 LMercury[5]  B        811.765                        1000 
-    19 LMercury[6]  B        632.184                        1000 
-    20 LMercury[7]  NU          1000                        1000             1 
-    21 LMercury[8]  NU          1000                        1000             1 
-    22 LMercury[9]  NU          1000                        1000       1.23529 
-    23 LMercury[10] B            900                        1000 
-    24 LMercury[11] B            700                        1000 
-    25 LMercury[12] B            600                        1000 
-    26 LVenus[1]    NU          1000                        1000       1.42857 
-    27 LVenus[2]    B           1000                        1000 
-    28 LVenus[3]    B           1000                        1000 
-    29 LVenus[4]    B           1000                        1000 
-    30 LVenus[5]    NU          1000                        1000       1.32353 
-    31 LVenus[6]    NU          1000                        1000       1.71264 
-    32 LVenus[7]    NU          1000                        1000             1 
-    33 LVenus[8]    NU          1000                        1000             1 
-    34 LVenus[9]    B        882.353                        1000 
-    35 LVenus[10]   B           1000                        1000 
-    36 LVenus[11]   B           1000                        1000 
-    37 LVenus[12]   B           1000                        1000 
+     1 revenue      B      1.127e+06                             
+     2 LMars[1]     NU          1000                        1000            23 
+     3 LMars[2]     NU          1000                        1000            35 
+     4 LMars[3]     NU          1000                        1000            38 
+     5 LMars[4]     NU          1000                        1000            42 
+     6 LMars[5]     NU          1000                        1000            42 
+     7 LMars[6]     NU          1000                        1000            50 
+     8 LMars[7]     NU          1000                        1000            62 
+     9 LMars[8]     NU          1000                        1000            65 
+    10 LMars[9]     NU          1000                        1000            45 
+    11 LMars[10]    NU          1000                        1000            43 
+    12 LMars[11]    NU          1000                        1000            30 
+    13 LMars[12]    NU          1000                        1000            22 
+    14 LMercury[1]  NU          1000                        1000            13 
+    15 LMercury[2]  NU          1000                        1000            25 
+    16 LMercury[3]  NU          1000                        1000            28 
+    17 LMercury[4]  NU          1000                        1000            32 
+    18 LMercury[5]  NU          1000                        1000            32 
+    19 LMercury[6]  NU          1000                        1000            40 
+    20 LMercury[7]  NU          1000                        1000            52 
+    21 LMercury[8]  NU          1000                        1000            75 
+    22 LMercury[9]  NU          1000                        1000            35 
+    23 LMercury[10] NU          1000                        1000            33 
+    24 LMercury[11] NU          1000                        1000            20 
+    25 LMercury[12] NU          1000                        1000            12 
+    26 LVenus[1]    NU          1000                        1000            10 
+    27 LVenus[2]    NU          1000                        1000            11 
+    28 LVenus[3]    NU          1000                        1000            10 
+    29 LVenus[4]    NU          1000                        1000            12 
+    30 LVenus[5]    NU          1000                        1000            13 
+    31 LVenus[6]    NU          1000                        1000            22 
+    32 LVenus[7]    NU          1000                        1000            35 
+    33 LVenus[8]    NU          1000                        1000            35 
+    34 LVenus[9]    NU          1000                        1000            30 
+    35 LVenus[10]   NU          1000                        1000            23 
+    36 LVenus[11]   NU          1000                        1000            20 
+    37 LVenus[12]   NU          1000                        1000            12 
 
    No. Column name  St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 x[1,R]       B        71.4286             0               
-     2 x[1,C]       NL             0             0                    -4.14286 
-     3 x[1,I]       B        14.2857             0               
-     4 x[2,R]       B            100             0               
-     5 x[2,C]       NL             0             0                       -29.5 
-     6 x[2,I]       NL             0             0                         -13 
-     7 x[3,R]       B            100             0               
-     8 x[3,C]       NL             0             0                       -28.6 
-     9 x[3,I]       NL             0             0                         -14 
-    10 x[4,R]       B            100             0               
-    11 x[4,C]       NL             0             0                       -35.8 
-    12 x[4,I]       NL             0             0                         -16 
-    13 x[5,R]       B        29.4118             0               
-    14 x[5,C]       NL             0             0                    -18.8588 
-    15 x[5,I]       B        35.2941             0               
-    16 x[6,R]       B        11.4943             0               
-    17 x[6,C]       NL             0             0                    -4.48276 
-    18 x[6,I]       B        25.2874             0               
-    19 x[7,R]       B        36.7647             0               
-    20 x[7,C]       B        7.35294             0               
-    21 x[7,I]       B        3.67647             0               
-    22 x[8,R]       B        32.1543             0               
-    23 x[8,C]       B        7.71704             0               
-    24 x[8,I]       B        4.50161             0               
-    25 x[9,R]       B        47.0588             0               
-    26 x[9,C]       B        11.7647             0               
-    27 x[9,I]       NL             0             0                    -6.47059 
-    28 x[10,R]      B            100             0               
-    29 x[10,C]      NL             0             0                       -25.7 
-    30 x[10,I]      NL             0             0                         -17 
-    31 x[11,R]      B            100             0               
-    32 x[11,C]      NL             0             0                         -11 
-    33 x[11,I]      NL             0             0                       -12.5 
-    34 x[12,R]      B            100             0               
-    35 x[12,C]      NL             0             0                         -12 
-    36 x[12,I]      NL             0             0                       -11.2 
+     1 x[1,R]       NL             0             0                         -13 
+     2 x[1,C]       B           1000             0               
+     3 x[1,I]       NL             0             0                          -3 
+     4 x[2,R]       NL             0             0                         -25 
+     5 x[2,C]       B           1000             0               
+     6 x[2,I]       NL             0             0                         -15 
+     7 x[3,R]       NL             0             0                         -28 
+     8 x[3,C]       B           1000             0               
+     9 x[3,I]       NL             0             0                         -18 
+    10 x[4,R]       NL             0             0                         -32 
+    11 x[4,C]       B           1000             0               
+    12 x[4,I]       NL             0             0                         -22 
+    13 x[5,R]       NL             0             0                         -32 
+    14 x[5,C]       B           1000             0               
+    15 x[5,I]       NL             0             0                         -22 
+    16 x[6,R]       NL             0             0                         -40 
+    17 x[6,C]       B           1000             0               
+    18 x[6,I]       NL             0             0                         -15 
+    19 x[7,R]       NL             0             0                         -52 
+    20 x[7,C]       NL             0             0                          -7 
+    21 x[7,I]       B           1000             0               
+    22 x[8,R]       NL             0             0                         -55 
+    23 x[8,C]       NL             0             0                         -15 
+    24 x[8,I]       B           1000             0               
+    25 x[9,R]       NL             0             0                         -35 
+    26 x[9,C]       B           1000             0               
+    27 x[9,I]       NL             0             0                         -15 
+    28 x[10,R]      NL             0             0                         -33 
+    29 x[10,C]      B           1000             0               
+    30 x[10,I]      NL             0             0                         -13 
+    31 x[11,R]      NL             0             0                         -20 
+    32 x[11,C]      B           1000             0               
+    33 x[11,I]      NL             0             0                          -5 
+    34 x[12,R]      NL             0             0                         -12 
+    35 x[12,C]      NL             0             0                          -2 
+    36 x[12,I]      B           1000             0               
+    37 x1[1,R]      NL             0             0                          -1 
+    38 x1[1,C]      B           1000             0               
+    39 x1[1,I]      NL             0             0                          -3 
+    40 x1[2,R]      NL             0             0                         -15 
+    41 x1[2,C]      B           1000             0               
+    42 x1[2,I]      NL             0             0                         -15 
+    43 x1[3,R]      NL             0             0                         -18 
+    44 x1[3,C]      B           1000             0               
+    45 x1[3,I]      NL             0             0                         -18 
+    46 x1[4,R]      NL             0             0                         -22 
+    47 x1[4,C]      B           1000             0               
+    48 x1[4,I]      NL             0             0                         -22 
+    49 x1[5,R]      NL             0             0                         -10 
+    50 x1[5,C]      B           1000             0               
+    51 x1[5,I]      NL             0             0                         -22 
+    52 x1[6,R]      NL             0             0                          -8 
+    53 x1[6,C]      B           1000             0               
+    54 x1[6,I]      NL             0             0                         -15 
+    55 x1[7,R]      NL             0             0                         -39 
+    56 x1[7,C]      NL             0             0                          -7 
+    57 x1[7,I]      B           1000             0               
+    58 x1[8,R]      NL             0             0                         -64 
+    59 x1[8,C]      NL             0             0                         -35 
+    60 x1[8,I]      B           1000             0               
+    61 x1[9,R]      NL             0             0                         -25 
+    62 x1[9,C]      B           1000             0               
+    63 x1[9,I]      NL             0             0                         -15 
+    64 x1[10,R]     NL             0             0                         -23 
+    65 x1[10,C]     B           1000             0               
+    66 x1[10,I]     NL             0             0                         -13 
+    67 x1[11,R]     NL             0             0                         -10 
+    68 x1[11,C]     B           1000             0               
+    69 x1[11,I]     NL             0             0                          -5 
+    70 x1[12,R]     NL             0             0                          -2 
+    71 x1[12,C]     NL             0             0                          -2 
+    72 x1[12,I]     B           1000             0               
+    73 x2[1,R]      NL             0             0                          -2 
+    74 x2[1,C]      NL             0             0                          -2 
+    75 x2[1,I]      B           1000             0               
+    76 x2[2,R]      NL             0             0                          -4 
+    77 x2[2,C]      NL             0             0                          -6 
+    78 x2[2,I]      B           1000             0               
+    79 x2[3,R]      NL             0             0                          -3 
+    80 x2[3,C]      NL             0             0                          -2 
+    81 x2[3,I]      B           1000             0               
+    82 x2[4,R]      NL             0             0                          -3 
+    83 x2[4,C]      B           1000             0               
+    84 x2[4,I]      NL             0             0                       < eps
+    85 x2[5,R]      NL             0             0                          -1 
+    86 x2[5,C]      NL             0             0                          -1 
+    87 x2[5,I]      B           1000             0               
+    88 x2[6,R]      B           1000             0               
+    89 x2[6,C]      NL             0             0                          -2 
+    90 x2[6,I]      NL             0             0                          -7 
+    91 x2[7,R]      NL             0             0                         -17 
+    92 x2[7,C]      B           1000             0               
+    93 x2[7,I]      NL             0             0                         -13 
+    94 x2[8,R]      NL             0             0                         -16 
+    95 x2[8,C]      NL             0             0                          -5 
+    96 x2[8,I]      B           1000             0               
+    97 x2[9,R]      NL             0             0                         -15 
+    98 x2[9,C]      NL             0             0                          -5 
+    99 x2[9,I]      B           1000             0               
+   100 x2[10,R]     NL             0             0                         -14 
+   101 x2[10,C]     B           1000             0               
+   102 x2[10,I]     NL             0             0                          -3 
+   103 x2[11,R]     NL             0             0                         -13 
+   104 x2[11,C]     B           1000             0               
+   105 x2[11,I]     NL             0             0                          -5 
+   106 x2[12,R]     NL             0             0                          -6 
+   107 x2[12,C]     NL             0             0                          -2 
+   108 x2[12,I]     B           1000             0               
 
 Karush-Kuhn-Tucker optimality conditions:
 
-KKT.PE: max.abs.err = 2.27e-13 on row 8
-        max.rel.err = 1.14e-16 on row 8
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
         High quality
 
 KKT.PB: max.abs.err = 0.00e+00 on row 0
         max.rel.err = 0.00e+00 on row 0
         High quality
 
-KKT.DE: max.abs.err = 2.84e-14 on column 20
-        max.rel.err = 1.35e-16 on column 26
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
         High quality
 
 KKT.DB: max.abs.err = 0.00e+00 on row 0

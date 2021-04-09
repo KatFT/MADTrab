1cb6a14e060aea7435f0253b484c2f95fbb9fbe8
Catarina Teixeira <up201805042@fc.up.pt>
2021-04-09 22:12:01 +0100

    fim

---
+++ up201805042up201805437/ex1-3.sol
@@ -0,0 +1,393 @@
+Problem:    ex1
+Rows:       73
+Columns:    108
+Non-zeros:  540
+Status:     OPTIMAL
+Objective:  lucro = 492965.7895 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 lucro        B         492966                             
+     2 M_Mandados[1,Venus]
+                    B        308.333                        1000 
+     3 M_Mandados[1,Mars]
+                    NU          1000                        1000           1.5 
+     4 M_Mandados[1,Mercury]
+                    B              0                        1000 
+     5 M_Mandados[2,Venus]
+                    B              0                        1000 
+     6 M_Mandados[2,Mars]
+                    B            850                        1000 
+     7 M_Mandados[2,Mercury]
+                    B              0                        1000 
+     8 M_Mandados[3,Venus]
+                    B              0                        1000 
+     9 M_Mandados[3,Mars]
+                    B            850                        1000 
+    10 M_Mandados[3,Mercury]
+                    B              0                        1000 
+    11 M_Mandados[4,Venus]
+                    B              0                        1000 
+    12 M_Mandados[4,Mars]
+                    B            850                        1000 
+    13 M_Mandados[4,Mercury]
+                    B              0                        1000 
+    14 M_Mandados[5,Venus]
+                    B         421.93                        1000 
+    15 M_Mandados[5,Mars]
+                    B         656.14                        1000 
+    16 M_Mandados[5,Mercury]
+                    B              0                        1000 
+    17 M_Mandados[6,Venus]
+                    B         421.93                        1000 
+    18 M_Mandados[6,Mars]
+                    B         656.14                        1000 
+    19 M_Mandados[6,Mercury]
+                    B              0                        1000 
+    20 M_Mandados[7,Venus]
+                    B            200                        1000 
+    21 M_Mandados[7,Mars]
+                    NU          1000                        1000            10 
+    22 M_Mandados[7,Mercury]
+                    B              0                        1000 
+    23 M_Mandados[8,Venus]
+                    NU          1000                        1000            10 
+    24 M_Mandados[8,Mars]
+                    B            200                        1000 
+    25 M_Mandados[8,Mercury]
+                    B              0                        1000 
+    26 M_Mandados[9,Venus]
+                    B              0                        1000 
+    27 M_Mandados[9,Mars]
+                    B            850                        1000 
+    28 M_Mandados[9,Mercury]
+                    B              0                        1000 
+    29 M_Mandados[10,Venus]
+                    B              0                        1000 
+    30 M_Mandados[10,Mars]
+                    B            850                        1000 
+    31 M_Mandados[10,Mercury]
+                    B              0                        1000 
+    32 M_Mandados[11,Venus]
+                    B        308.333                        1000 
+    33 M_Mandados[11,Mars]
+                    NU          1000                        1000       9.58333 
+    34 M_Mandados[11,Mercury]
+                    B              0                        1000 
+    35 M_Mandados[12,Venus]
+                    B        308.333                        1000 
+    36 M_Mandados[12,Mars]
+                    NU          1000                        1000       6.58333 
+    37 M_Mandados[12,Mercury]
+                    B              0                        1000 
+    38 Cap[1,Cleaning]
+                    B       0.879762                           1 
+    39 Cap[1,Cooking]
+                    NU             1                           1         22200 
+    40 Cap[1,Packing]
+                    B       0.911111                           1 
+    41 Cap[2,Cleaning]
+                    B       0.553746                           1 
+    42 Cap[2,Cooking]
+                    NU             1                           1         29750 
+    43 Cap[2,Packing]
+                    B       0.566667                           1 
+    44 Cap[3,Cleaning]
+                    B       0.553746                           1 
+    45 Cap[3,Cooking]
+                    NU             1                           1         32300 
+    46 Cap[3,Packing]
+                    B       0.566667                           1 
+    47 Cap[4,Cleaning]
+                    B       0.553746                           1 
+    48 Cap[4,Cooking]
+                    NU             1                           1         35700 
+    49 Cap[4,Packing]
+                    B       0.566667                           1 
+    50 Cap[5,Cleaning]
+                    B       0.849383                           1 
+    51 Cap[5,Cooking]
+                    NU             1                           1       34208.8 
+    52 Cap[5,Packing]
+                    NU             1                           1       2631.58 
+    53 Cap[6,Cleaning]
+                    B       0.849383                           1 
+    54 Cap[6,Cooking]
+                    NU             1                           1       37519.3 
+    55 Cap[6,Packing]
+                    NU             1                           1       8789.47 
+    56 Cap[7,Cleaning]
+                    B       0.685714                           1 
+    57 Cap[7,Cooking]
+                    NU             1                           1         62400 
+    58 Cap[7,Packing]
+                    B            0.6                           1 
+    59 Cap[8,Cleaning]
+                    B       0.685714                           1 
+    60 Cap[8,Cooking]
+                    NU             1                           1         78000 
+    61 Cap[8,Packing]
+                    B            0.6                           1 
+    62 Cap[9,Cleaning]
+                    B       0.553746                           1 
+    63 Cap[9,Cooking]
+                    NU             1                           1         38250 
+    64 Cap[9,Packing]
+                    B       0.566667                           1 
+    65 Cap[10,Cleaning]
+                    B       0.553746                           1 
+    66 Cap[10,Cooking]
+                    NU             1                           1         36550 
+    67 Cap[10,Packing]
+                    B       0.566667                           1 
+    68 Cap[11,Cleaning]
+                    B       0.879762                           1 
+    69 Cap[11,Cooking]
+                    NU             1                           1         18500 
+    70 Cap[11,Packing]
+                    B       0.911111                           1 
+    71 Cap[12,Cleaning]
+                    B       0.879762                           1 
+    72 Cap[12,Cooking]
+                    NU             1                           1         18500 
+    73 Cap[12,Packing]
+                    B       0.911111                           1 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 vendeu[Venus,R,1]
+                    B        308.333             0               
+     2 vendeu[Venus,C,1]
+                    NL             0             0                    -13.1176 
+     3 vendeu[Venus,I,1]
+                    NL             0             0                        -8.5 
+     4 vendeu[Mars,R,1]
+                    NL             0             0                        -3.5 
+     5 vendeu[Mars,C,1]
+                    NL             0             0                    -4.61765 
+     6 vendeu[Mars,I,1]
+                    B           1000             0               
+     7 vendeu[Mercury,R,1]
+                    NL             0             0                          -4 
+     8 vendeu[Mercury,C,1]
+                    NL             0             0                    -18.1176 
+     9 vendeu[Mercury,I,1]
+                    NL             0             0                        -8.5 
+    10 vendeu[Venus,R,2]
+                    NL             0             0                    -6.08108 
+    11 vendeu[Venus,C,2]
+                    NL             0             0                         -10 
+    12 vendeu[Venus,I,2]
+                    NL             0             0                    -14.7917 
+    13 vendeu[Mars,R,2]
+                    NL             0             0                    -6.08108 
+    14 vendeu[Mars,C,2]
+                    B            850             0               
+    15 vendeu[Mars,I,2]
+                    NL             0             0                    -4.79167 
+    16 vendeu[Mercury,R,2]
+                    NL             0             0                    -9.08108 
+    17 vendeu[Mercury,C,2]
+                    NL             0             0                         -30 
+    18 vendeu[Mercury,I,2]
+                    NL             0             0                    -13.7917 
+    19 vendeu[Venus,R,3]
+                    NL             0             0                    -7.45946 
+    20 vendeu[Venus,C,3]
+                    NL             0             0                         -10 
+    21 vendeu[Venus,I,3]
+                    NL             0             0                    -16.9167 
+    22 vendeu[Mars,R,3]
+                    NL             0             0                    -7.45946 
+    23 vendeu[Mars,C,3]
+                    B            850             0               
+    24 vendeu[Mars,I,3]
+                    NL             0             0                    -6.91667 
+    25 vendeu[Mercury,R,3]
+                    NL             0             0                    -10.4595 
+    26 vendeu[Mercury,C,3]
+                    NL             0             0                         -30 
+    27 vendeu[Mercury,I,3]
+                    NL             0             0                    -16.9167 
+    28 vendeu[Venus,R,4]
+                    NL             0             0                     -9.2973 
+    29 vendeu[Venus,C,4]
+                    NL             0             0                         -10 
+    30 vendeu[Venus,I,4]
+                    NL             0             0                      -19.75 
+    31 vendeu[Mars,R,4]
+                    NL             0             0                     -9.2973 
+    32 vendeu[Mars,C,4]
+                    B            850             0               
+    33 vendeu[Mars,I,4]
+                    NL             0             0                       -9.75 
+    34 vendeu[Mercury,R,4]
+                    NL             0             0                    -10.2973 
+    35 vendeu[Mercury,C,4]
+                    NL             0             0                         -30 
+    36 vendeu[Mercury,I,4]
+                    NL             0             0                      -17.75 
+    37 vendeu[Venus,R,5]
+                    B         421.93             0               
+    38 vendeu[Venus,C,5]
+                    NL             0             0                         -10 
+    39 vendeu[Venus,I,5]
+                    NL             0             0                    -19.8231 
+    40 vendeu[Mars,R,5]
+                    NL             0             0                         -12 
+    41 vendeu[Mars,C,5]
+                    B         656.14             0               
+    42 vendeu[Mars,I,5]
+                    NL             0             0                     -9.8231 
+    43 vendeu[Mercury,R,5]
+                    NL             0             0                         -10 
+    44 vendeu[Mercury,C,5]
+                    NL             0             0                         -30 
+    45 vendeu[Mercury,I,5]
+                    NL             0             0                    -16.8231 
+    46 vendeu[Venus,R,6]
+                    B         421.93             0               
+    47 vendeu[Venus,C,6]
+                    NL             0             0                         -10 
+    48 vendeu[Venus,I,6]
+                    NL             0             0                    -10.6608 
+    49 vendeu[Mars,R,6]
+                    NL             0             0                         -22 
+    50 vendeu[Mars,C,6]
+                    B         656.14             0               
+    51 vendeu[Mars,I,6]
+                    NL             0             0                   -0.660819 
+    52 vendeu[Mercury,R,6]
+                    NL             0             0                         -10 
+    53 vendeu[Mercury,C,6]
+                    NL             0             0                         -30 
+    54 vendeu[Mercury,I,6]
+                    NL             0             0                    -20.6608 
+    55 vendeu[Venus,R,7]
+                    NL             0             0                    -20.7297 
+    56 vendeu[Venus,C,7]
+                    NL             0             0                    -28.4118 
+    57 vendeu[Venus,I,7]
+                    B            200             0               
+    58 vendeu[Mars,R,7]
+                    NL             0             0                    -33.7297 
+    59 vendeu[Mars,C,7]
+                    NL             0             0                    -28.4118 
+    60 vendeu[Mars,I,7]
+                    B           1000             0               
+    61 vendeu[Mercury,R,7]
+                    NL             0             0                    -15.7297 
+    62 vendeu[Mercury,C,7]
+                    NL             0             0                    -38.4118 
+    63 vendeu[Mercury,I,7]
+                    NL             0             0                         -30 
+    64 vendeu[Venus,R,8]
+                    NL             0             0                    -41.1622 
+    65 vendeu[Venus,C,8]
+                    NL             0             0                    -61.7647 
+    66 vendeu[Venus,I,8]
+                    B           1000             0               
+    67 vendeu[Mars,R,8]
+                    NL             0             0                    -32.1622 
+    68 vendeu[Mars,C,8]
+                    NL             0             0                    -41.7647 
+    69 vendeu[Mars,I,8]
+                    B            200             0               
+    70 vendeu[Mercury,R,8]
+                    NL             0             0                    -23.1622 
+    71 vendeu[Mercury,C,8]
+                    NL             0             0                    -61.7647 
+    72 vendeu[Mercury,I,8]
+                    NL             0             0                         -30 
+    73 vendeu[Venus,R,9]
+                    NL             0             0                    -10.6757 
+    74 vendeu[Venus,C,9]
+                    NL             0             0                         -10 
+    75 vendeu[Venus,I,9]
+                    NL             0             0                     -11.875 
+    76 vendeu[Mars,R,9]
+                    NL             0             0                    -10.6757 
+    77 vendeu[Mars,C,9]
+                    B            850             0               
+    78 vendeu[Mars,I,9]
+                    NL             0             0                      -1.875 
+    79 vendeu[Mercury,R,9]
+                    NL             0             0                    -5.67568 
+    80 vendeu[Mercury,C,9]
+                    NL             0             0                         -20 
+    81 vendeu[Mercury,I,9]
+                    NL             0             0                      -1.875 
+    82 vendeu[Venus,R,10]
+                    NL             0             0                    -9.75676 
+    83 vendeu[Venus,C,10]
+                    NL             0             0                         -10 
+    84 vendeu[Venus,I,10]
+                    NL             0             0                    -10.4583 
+    85 vendeu[Mars,R,10]
+                    NL             0             0                    -9.75676 
+    86 vendeu[Mars,C,10]
+                    B            850             0               
+    87 vendeu[Mars,I,10]
+                    NL             0             0                   -0.458333 
+    88 vendeu[Mercury,R,10]
+                    NL             0             0                    -10.7568 
+    89 vendeu[Mercury,C,10]
+                    NL             0             0                         -20 
+    90 vendeu[Mercury,I,10]
+                    NL             0             0                    -10.4583 
+    91 vendeu[Venus,R,11]
+                    B        308.333             0               
+    92 vendeu[Venus,C,11]
+                    NL             0             0                    -1.76471 
+    93 vendeu[Venus,I,11]
+                    NL             0             0                   -0.416667 
+    94 vendeu[Mars,R,11]
+                    NL             0             0                    -9.58333 
+    95 vendeu[Mars,C,11]
+                    NL             0             0                    -1.34804 
+    96 vendeu[Mars,I,11]
+                    B           1000             0               
+    97 vendeu[Mercury,R,11]
+                    NL             0             0                          -3 
+    98 vendeu[Mercury,C,11]
+                    NL             0             0                    -1.76471 
+    99 vendeu[Mercury,I,11]
+                    NL             0             0                   -0.416667 
+   100 vendeu[Venus,R,12]
+                    B        308.333             0               
+   101 vendeu[Venus,C,12]
+                    NL             0             0                    -11.7647 
+   102 vendeu[Venus,I,12]
+                    NL             0             0                    -3.41667 
+   103 vendeu[Mars,R,12]
+                    NL             0             0                    -6.58333 
+   104 vendeu[Mars,C,12]
+                    NL             0             0                    -8.34804 
+   105 vendeu[Mars,I,12]
+                    B           1000             0               
+   106 vendeu[Mercury,R,12]
+                    NL             0             0                          -4 
+   107 vendeu[Mercury,C,12]
+                    NL             0             0                    -11.7647 
+   108 vendeu[Mercury,I,12]
+                    NL             0             0                    -3.41667 
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 1.14e-13 on row 3
+        max.rel.err = 5.68e-17 on row 3
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 1.42e-14 on column 66
+        max.rel.err = 1.08e-16 on column 69
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
